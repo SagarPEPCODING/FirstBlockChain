f43918ae1eceee941af5a8edf66795c60b55dbca
sagarPEPCODING <raaasamrahs1234@gmail.com>
2022-01-13 01:06:38 +0530

    12 Jan 2022

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,5 +1,6 @@
 const Wallet = require("./index");
 const { verifySignature } = require("../util");
+const Transaction = require("./transaction");
 describe("Wallet", () => {
   let wallet;
 
@@ -34,6 +35,36 @@ describe("Wallet", () => {
           signature: new Wallet().sign(data),
         })
       ).toBe(false);
+    });
+  });
+
+  describe("createTransaction", () => {
+    describe("and the amount exceeds the balance", () => {
+      it("throws an error", () => {
+        expect(() =>
+          wallet.createTransaction({ amount: 999999, recipent: "foo-recipent" })
+        ).toThrow("Amount exceeds balance...");
+      });
+    });
+
+    describe("amount is valid", () => {
+      let transaction, amount, recipient;
+      beforeEach(() => {
+        amount = 50;
+        recipient = "foo-recipient";
+        transaction = wallet.createTransaction({ amount, recipient });
+      });
+      it("create an instance of transaction", () => {
+        expect(transaction instanceof Transaction).toBe(true);
+      });
+
+      it("matches the transaction input with the wallet", () => {
+        expect(transaction.input.address).toEqual(wallet.publicKey);
+      });
+
+      it("outputs the amount the recipent", () => {
+        expect(transaction.outputMap[recipient]).toEqual(amount);
+      });
     });
   });
 });
